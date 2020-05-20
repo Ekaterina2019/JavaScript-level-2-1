7fa93439bc76f973b5b0db23bf9d498badf05288
Ekaterina Shestakova <shlikova93@gmail.com>
2020-05-20 17:32:37 +0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const products = [
+    {id: 1, title: 'Notebook', price: 2000},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 200},
+    {id: 4, title: 'Gamepad', price: 50}
+]
+
+const product={
+    init(products){
+        this.renderPage (products)
+    },
+    renderPage (list){
+        for (let i=0;i<list.length;i++){
+            const productsList = this.renderProduct(list[i].title, list[i].price)
+            const product = document.querySelector('.products');
+            product.insertAdjacentHTML("beforeend", productsList);
+        }
+    },
+    renderProduct(title = 'Notebook', price = 2000){
+        return `<div class="product-item">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`}
+}
+
+product.init(products)
